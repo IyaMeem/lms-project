f4e3dbf05aae604f7062da97521f5676995cffef
IyaMeem <iyameem93@gmail.com>
2023-01-06 22:43:57 +0600

    lms-project

---
+++ app/Models/Curri.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curri extends Model
+{
+    use HasFactory;
+}
