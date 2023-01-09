973eb370c5e85252306aaed9785f4ac38ef95eba
IyaMeem <53008931+IyaMeem@users.noreply.github.com>
2023-01-10 02:23:13 +0600

    #7

--- app/Http/Livewire/LeadEdit.php
+++ app/Http/Livewire/LeadEdit.php
@@ -4,16 +4,64 @@ namespace App\Http\Livewire;
 
 use App\Models\Lead;
 use Livewire\Component;
+use App\Models\Note;
+use App\Models\Note as ModelsNote;
 
 class LeadEdit extends Component
 {
     public $lead_id;
+    public $name;
+    public $email;
+    public $phone;
+
+    public $note;
+
+    public function mount() {
+        $lead = Lead::findOrFail($this->lead_id);
+        $this->lead_id = $lead->id;
+        $this->name = $lead->name;
+        $this->email = $lead->email;
+        $this->phone = $lead->phone;
+    }
+
 
     public function render()
     {
         $lead = Lead::findOrFail($this->lead_id);
+
         return view('livewire.lead-edit', [
-            'lead' => $lead
+            'notes' => $lead->notes
         ]);
+    }
+
+    protected $rules = [
+        'email' => 'email',
+        'phone' => 'required',
+    ];
+
+    public function submitForm() {
+        sleep(3);
+
+        $lead = Lead::findOrFail($this->lead_id);
+
+        $this->validate();
+
+        $lead->name = $this->name;
+        $lead->email = $this->email;
+        $lead->phone = $this->phone;
+        $lead->save();
+
+        flash()->addSuccess('Lead deteted successfuly');
+    }
+
+    public function addNote() {
+        $note = new Note();
+        $note->description = $this->note;
+        $note->lead_id = $this->lead_id;
+        $note->save();
+
+        $this->note = '';
+
+        flash()->addSuccess('Note added successfully');
     }
 }
