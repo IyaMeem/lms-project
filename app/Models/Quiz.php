f37801d76e89f3ed63f78c19d1e0640da8ade8e8
IyaMeem <53008931+IyaMeem@users.noreply.github.com>
2023-01-18 04:30:11 +0600

    #14

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function questions() {
+        return $this->belongsToMany(Question::class, 'quiz_question', 'quiz_id', 'question_id');
+    }
 }
